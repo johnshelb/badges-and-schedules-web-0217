1f8a31ca2ea28e86133bc304babcbe5decd09cb9
John Shelby <jsnd11@gmail.com>
2016-12-29 16:56:25 -0500

    conference ready

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  b=[]
+  array.each{|name| b<<"Hello, my name is #{name}."}
+  return b
+end
+
+def assign_rooms(array)
+  b=[]
+  array.each_with_index{|name,index| b<<"Hello, #{name}! You'll be assigned to room #{index+1}!"}
+  return b
+end
+
+def printer(array)
+  listofnames=batch_badge_creator(array)
+  roomlist=assign_rooms(array)
+  for x in (0...array.count)
+    puts listofnames[x]
+    puts roomlist[x]
+  end
+end
